b44bac499da7f0e7951db60a7c58633b365e5f8c
ayekgun <ayekgun1@gmail.com>
2021-07-19 11:50:09 +0700

    isi no hp dan otp via whatsapp

--- percobaan.py
+++ percobaan.py
@@ -1,5 +1,6 @@
 from appium import  webdriver
 
+# desired capability
 desired_cap = {
     "platformName": "Android",
     "deviceName": "emulator-5554",
@@ -8,9 +9,24 @@ desired_cap = {
     "appWaitActivity": "tokko.onlineshop.jual.mitra.app.MainActivity"
 }
 
+# remote appium server
 drivur = webdriver.Remote("http://localhost:4723/wd/hub", desired_cap)
 
+# tunggu 30 mlidetik agar element muncul
 drivur.implicitly_wait(30)
+
+# tekan button daftar
 el = drivur.find_element_by_accessibility_id('uspSplashPage_getStartedButton')
 el.click()
+
+# masukan no hp
+pi = drivur.find_element_by_accessibility_id('loginPage_phoneNumberInput')
+pi.send_keys('082229720709')
+
+# otp via whatsapp
+ot = drivur.find_element_by_accessibility_id('loginPage_whatsappLoginButton')
+ot.click()
+
+
+
 
